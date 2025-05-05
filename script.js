954309f24d6c812f03e2a9df3a7328e214b48bc4
Shaik Tippu Sultan <shaiktippu296@gmail.com>
2025-05-05 23:20:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let item = document.querySelector(".item")
+
+        function openPop() {
+            item.classList.add("item-click")
+        }
+
+        function closePop() {
+            item.classList.remove("item-click")
+        }
